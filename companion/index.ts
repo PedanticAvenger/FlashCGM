2e91089ec634c09eb84f051309979b5bcb72ab2e
raymond-richmond <raymond.richmond@gmail.com>
2018-03-12 16:05:51 -0600

    Code tweaks to make tslint happy.

--- companion/index.ts
+++ companion/index.ts
@@ -32,11 +32,11 @@ messaging.peerSocket.close = () => {
 
 const dataPoll = () => {
   if(sendAllData) {
-      console.log('Grabbing Settings.')
+      console.log("Grabbing Settings.")
       settingsPoll();
       sendAllData = false;
   }
-  console.log('Open Data API CONNECTION')
+  console.log("Open Data API CONNECTION")
   console.log(url)
   if(url) {
     fetch(url,{
@@ -52,29 +52,28 @@ const dataPoll = () => {
           //sendVal(data);
           buildGraphData(data);
         }).catch(responseParsingError => {
-          console.log('fetchError1');
+          console.log("Response parsing error in data!");
           console.log(responseParsingError.name);
           console.log(responseParsingError.message);
           console.log(responseParsingError.toString());
           console.log(responseParsingError.stack);
         });
       }).catch(fetchError => {
-        console.log('fetchError2');
+        console.log("Fetch error in data!");
         console.log(fetchError.name);
         console.log(fetchError.message);
         console.log(fetchError.toString());
         console.log(fetchError.stack);
       })
   } else {
-    console.log('no url stored in settings to use to get data.')
+    console.log("no url stored in settings to use to get data.")
   }
 }
 
 const settingsPoll = () => {
-  console.log('Open Settings API CONNECTION')
+  console.log("Open Settings API CONNECTION")
   console.log(settingsUrl)
   if(settingsUrl) {
-    //url = url + "?count=14";
     fetch(settingsUrl,{
       method: 'GET',
       mode: 'cors',
@@ -83,27 +82,27 @@ const settingsPoll = () => {
       })
     })
       .then(response => {
-        //debug logging console.log('Get Data From Phone');
+        //debug logging console.log('Get Settings From Phone');
         response.text().then(settings => {
-          //debug logging console.log('fetched Data from API');
+          //debug logging console.log('fetched settings from API');
           buildSettings(settings);
         })
         .catch(responseParsingError => {
-          console.log('fetchError1');
+          console.log("Response parsing error in settings!");
           console.log(responseParsingError.name);
           console.log(responseParsingError.message);
           console.log(responseParsingError.toString());
           console.log(responseParsingError.stack);
         });
       }).catch(fetchError => {
-        console.log('fetchError2');
+        console.log("Fetch error in settings!");
         console.log(fetchError.name);
         console.log(fetchError.message);
         console.log(fetchError.toString());
         console.log(fetchError.stack);
       })
   } else {
-    console.log('no url stored in app settings to use to get settings.')
+    console.log("no url stored in app settings to use to get settings.")
   }
 };
 
@@ -216,10 +215,9 @@ function restoreSettings() {
 
 // Ok, so we will be having various message types going back and forth to the watch.
 // Should we set a flag in the data bundle of each message to modularize the processing on the watch-side?
-// Also, currently this is inherited from flashring which only sends theme info so it needs to be updated.
 // After using until March 6, decided to move all processing here and make watch display/trigger.
 // the only math I want the watch to do is calculate time since last reading so it is "independant" from watch and app message interactions.
-// Will create:
+// Created/ing:
 // an array with 24 data points for graphing (leave in mg/dl format)
 // units variable for display
 // variable for current value in appropriate display units
@@ -243,28 +241,26 @@ settingsStorage.onchange = function(evt) {
 
 /*
   Workflow thoughts:
-  -Grab configured high and low and units from endpoints in XDrip/Nightscout to be used to set the graph lines as well as for triggering vibe alerts.
-  -Grab JSON response from defined data source URL saved in settings (likely http://127.0.0.1:17850/sgv.json).
-  -Look for units_hint in the first record and use that to determine required calculations and set units on locally stored settings just because.
-    -If no units_hint returned look for the internally stored value
+  -Done: Grab configured high and low and units from endpoints in XDrip/Nightscout to be used to set the graph lines as well as for triggering vibe alerts.
+  - Currently debugging: Grab JSON response from defined data source URL saved in settings (likely http://127.0.0.1:17850/sgv.json).
+  
   Messages:
-  1)array of data points for graph, 24 BG values in appropriate units
-  2)Current reading, trend at most current reading, appropriate timestamp of most recent result,
-  3) Units, configured high and configured low
-  4) Theme messages from flashring with incorporated theme settings for display data.
+  1)array of data points for graph (24 BG values in mg units), along with trend at most recent value along with its timestamp
+  2) Units, configured high and configured low
+  3) Theme messages from flashring with incorporated theme settings for display data.
 
-    -At this point I'm leaning toward a simple ring that uses color to count out 5 minutes then print a number in center of ring for number of missed polls.
+  "Age of readings":
+  At this point I'm leaning toward a simple ring that uses color to count out 5 minutes then print a number in center of ring for number of missed polls.
     Simpler for display/user understanding than "XXmYYs since last poll" kind of display but that could just be me
     and should be verified by some users with different perspectives.
-    -Trend data could be displayed as a 180 degree arc with a calculated arc segment on top for display.  fill of arc can change for extremes, etc.
+  Done:Trend data could be displayed as a 180 degree arc with a calculated arc segment on top for display.  fill of arc can change for extremes, etc.
+  
   -Grab the Heartrate (easiest to grab it from what is currently displayed on the watchface)  and steps (today.local.steps) and send them back to companion as a message.
   -Companion makes a second WebAPI call with sgv.json?steps=StepsValue&heart=HeartRateValue
     -Look for the JSON resonse for steps_result and heart_result to be 200 indicating success, everything else for a value is some form of error.
-      Possible mis-alignment of data points with the timing here but in all honesty we are talking about an interval so small it really doesn't matter I think.
-      Of course I say all the above now based on my trying to incorporate user-activity into the companion app and it doesn't seem to work that way so rather than a single web transaction to update Xdrip and get BG values we instead do it in two steps.
-
-
-   The more I look at this the more I'm certain that all the data processing from the JSON datasource needs to be done in the companion and simple messages of an array of glucose values, current trend, timestamp should be all that is sent.  Theme settings could be used to alter units display, etc.  Should in general be much more stable and battery friendly on the watch.
+      
+  Possible mis-alignment of data points with the timing here but in all honesty we are talking about an interval so small it really doesn't matter I think.
+  Of course I say all the above now based on my trying to incorporate user-activity into the companion app and it doesn't seem to work that way so rather than a single web transaction to update Xdrip and get BG values we instead do it in two steps.
 */
 
-setInterval(dataPoll, 300000); //Run every 5 min.
+setInterval(dataPoll, 150000); // Run every 2.5 min.
